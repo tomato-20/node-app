1e25cf1ac764d5e893f034d06cba37a83668c8bb
tomato-20 <programmer.anon.01.gmail.com>
2022-11-26 10:03:00 +0545

    fix: update update_at column on item update

--- src/models/Item.js
+++ src/models/Item.js
@@ -63,25 +63,28 @@ class Item {
   }
 
   static async update(id, updateInput) {
+    let date = new Date().toISOString().slice(0, 19).replace("T", " ");
+    let table = "UPDATE `items` ";
+    let whereCondition = "WHERE `id` = ?";
+
     try {
-      let table = "UPDATE `items` ";
-      let whereCondition = "WHERE `id` = ?";
-      const fields = Object.keys(updateInput);
-      const noOfFields = fields.length;
-      let set = fields.map((field, index) => {
-        return `\`${field}\`= ?` + (index < noOfFields - 1 ? ", " : " ");
-      });
-      let sql = table + 'SET ' + set.join("") + whereCondition +";";
+      if (Object.keys(updateInput).length) {
+        let set = Object.keys(updateInput)
+          .map((field) => `\`${field}\`= ?`)
+          .join(",");
+        set = set.concat(",`updated_at`= ? ");
+        let sql = table + "SET " + set +whereCondition + ";";
 
-      const values = Object.values(updateInput);
-      values.push(id);
+        const values = Object.values(updateInput);
+        values.push(...[date,id]);
 
-      await pool.execute(sql,values);
+        await pool.execute(sql, values);
+      }
       return;
     } catch (error) {
       throw error;
     } finally {
-      await pool.end()
+      // await pool.end();
     }
   }
 
